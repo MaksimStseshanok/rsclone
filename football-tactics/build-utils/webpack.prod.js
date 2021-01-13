4f5a09a5e7b9947e73226fea7122bcc933c062c6
Maksim Stseshanok <maksim.stseshanok@gmail.com>
2021-01-13 17:10:26 +0300

    feat: add webpack

---
+++ football-tactics/build-utils/webpack.prod.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const Dotenv = require('dotenv-webpack');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.(scss|css)$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'postcss-loader',
+          'sass-loader',
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin(),
+    new Dotenv({
+      path: path.resolve(__dirname, '..', './.env.production'),
+    }),
+  ],
+  devtool: 'source-map',
+};
